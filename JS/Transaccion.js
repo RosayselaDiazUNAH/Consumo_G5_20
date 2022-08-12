11b15e06e0ce0a325a9e51ee7ec15bc566a75d65
iselad <100822135+iselad@users.noreply.github.com>
2022-08-12 01:29:56 -0600

    Boton Agregar transacción
    Nueva transacción

--- JS/Transaccion.js
+++ JS/Transaccion.js
@@ -1,4 +1,5 @@
 var UrlTransacciones = 'http://20.216.41.245:90/G5_20/controller/Transaccion.php?opc=GetTransasaciones';
+var UrlInsertransaccion = 'http://20.216.41.245:90/G5_20/controller/Transaccion.php?opc=insertransaccion';
 
 $(document).ready(function(){
     CargarTransacciones();
@@ -20,13 +21,41 @@ function CargarTransacciones(){
                 '<td>'+ MiItems[i].FechaTransaccion +'</td>'  + 
                 '<td>'+ MiItems[i].MontoTransaccion +'</td>'  + 
                 '<td>'+ MiItems[i].Sucursal +'</td>'  + 
-                '<td>'+ MiItems[i].NumeroCuenta +'</td>'  + 
-              
-            '</tr>';
+                '<td>'+ MiItems[i].NumeroCuenta +'</td>'  +  
+                 '</tr>';
             $('#DataTransaccion').html(Valores);
             }
         }
     });
 }
-    
+function AgregarT(){
+    var datostransaccion = {
+    CodigoTransaccion: $('#CodigoTransaccion').val(),
+    TipoTransaccion: $('#TipoTransaccion').val(),
+    CodigoCliente: $('#CodigoCliente').val(),
+    FechaTransaccion: $('#FechaTransaccion').val(),
+    MontoTransaccion: $('#MontoTransaccion').val(),
+    Sucursal: $('#Sucursal').val(),
+    NumeroCuenta:$('#NumeroCuenta').val()
+    };
+    var datostransaccionjson = JSON.stringify(datostransaccion);
+
+    $.ajax({
+        url: UrlInsertransaccion,
+        type:'POST',
+        data: datostransaccionjson,
+        datatype: 'JSON',
+        contenttype: 'application/json',
+        success: function(reponse){
+            console.log(reponse);
+            alert('Transacción Agregado de Manera Exitosa');
+        },
+        error: function(textStatus, errorThrown){
+            alert('Error al agregar Transacción' + textStatus + errorThrown);
+        }
+    });
+    alert('Aviso')
+}
+
+   
 
